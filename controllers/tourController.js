8b42657eff90e59a216bdda484eb5097d60fdc2d
Yeasin Arafat <yeasinarafat@Air.local>
2024-02-24 00:05:59 +0600

    updating model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,79 +1,51 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkedID = (req, res, next, val) => {
-  console.log(`The id is ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
-    requestedAt: req.requestTime,
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
+    // requestedAt: req.requestTime,
+    // status: 'success',
+    // result: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+    const newTour = await Tour.create(req.body);
 
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tours: newTour,
-        },
-      });
-    }
-  );
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tours: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
   res.status(200).json({
-    status: 'success',
-    data: {
-      tours: tour,
-    },
+    // status: 'success',
+    // data: {
+    //   tours: tour,
+    // },
   });
 };
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updating tour here...>',
-    },
+    // status: 'success',
+    // data: {
+    //   tour: '<Updating tour here...>',
+    // },
   });
 };
 
