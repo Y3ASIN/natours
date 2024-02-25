fbbbdd761b11f28460bcb99b0b87971a77673f85
Yeasin Arafat <yeasinarafat@Air.local>
2024-02-25 20:19:07 +0600

    update model and data

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,26 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must contain a name'],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   rating: {
     type: Number,
     default: 4.5,
@@ -14,6 +34,26 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
