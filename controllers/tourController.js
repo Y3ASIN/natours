d83c142f0ff666c44d92762e4458d090e15c8b92
Yeasin Arafat <yeasinarafat@Air.local>
2024-03-03 11:23:57 +0600

    upload page limit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,6 +29,19 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+
+    // 4) Page limit
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page in not exist');
+    }
+
     //EXECUTE QUERY
     const tours = await query;
 
