70adc8c7434b99b46d0678ec051716ee1099a537
Y3ASIN <yeasinarafat.wa@gmail.com>
2024-11-02 16:47:59 +0600

    Module 10 - Authentication, Authorization & Security.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); 
 
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! Sutting down...');
